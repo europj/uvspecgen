e0fd1e0bae763aba438836e18f69d3141a76013e
Joseph May <jwmay87@gmail.com>
2013-05-19 21:14:37 -0700

    Add setup.py for distutils package distribution

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from uvspec import version as vrsn
+
+version = vrsn.get_version()
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='uvspecgen',
+      version=version,
+      py_modules=['lib.argparse'],
+      packages=['uvspec'],
+      scripts=['scripts/uvspecgen'],
+      description='Gaussian UV-Vis spectrum generation',
+      maintainer='Gaussian Toolkit',
+      maintainer_email='gaussiantoolkit@gmail.com',
+      url='http://github.com/gaussiantoolkit/uvspecgen/',
+      license='http://www.gnu.org/licenses/gpl.html',
+      long_description=long_description,
+     )
