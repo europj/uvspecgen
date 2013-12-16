cdea3449757a160a9799b8c5c8f0b628cc4468f2
Joseph May <jwmay87@gmail.com>
2013-12-16 00:44:06 -0800

    use setuptools for installation
    The setuptools allows for the installation of package dependencies such
    as cclib.

--- setup.py
+++ setup.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python
+#
+# Generate UV-Vis spectra from Gaussian09 TDHF/TDDFT log files. 
+# Copyright (C) 2013  Gaussian Toolkit
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from distutils.core import setup
+"""Installation script for the uvspec package and uvspecgen script.
+
+This installer also determines if the user has the cclib computational
+chemistry log file parsing library installed.  If not, it will perform that
+installation as well.
+
+"""
+from setuptools import setup
 from uvspec import get_version 
+
 
 version = get_version()
 
 with open('README.md') as f:
     long_description = f.read()
 
-setup(name='uvspecgen',
+
+setup(name='uvspec',
       version=version,
       packages=['uvspec', 'uvspec.config', 'uvspec.lib'],
       scripts=['uvspec/scripts/uvspecgen'],
       package_data={'uvspec': ['tests/*']},
+      install_requires=['cclib>=1.0'],
       description='Gaussian UV-Vis spectrum generation',
       maintainer='Li Research Group',
       maintainer_email='gaussiantoolkit@gmail.com',
